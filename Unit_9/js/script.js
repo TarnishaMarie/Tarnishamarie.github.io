449d8e79d63b8f2daba0c961e2a39f4f15ca4646
Tarnisha Washington <tarnishamarie@gmail.com>
2018-03-26 17:57:58 -0400

    unit 9

---
+++ Unit_9/js/script.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+$( "li" ).hover(
+  function() {
+    $( this ).append( $( "<span> ***</span>" ) );
+  }, function() {
+    $( this ).find( "span:last" ).remove();
+  }
+);
